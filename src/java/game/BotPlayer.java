8f3f2fc29816233f5a28528f4b367b295cb3dc9c
BayShubar <yerkebulan199@gmail.com>
2017-10-20 01:01:19 +0600

    add some intelect to Bot

--- src/java/game/BotPlayer.java
+++ src/java/game/BotPlayer.java
@@ -10,10 +10,83 @@ public class BotPlayer {
          this.boxes = boxes;
      }
      
+//     public int getBotMove(){
+//         for(int i=0; i<boxes.length; i++)
+//             if(boxes[i].getCondition() == 0)
+//                 return i;
+//         return 0;
+//     }
+     
      public int getBotMove(){
-         for(int i=0; i<boxes.length; i++)
-             if(boxes[i].getCondition() == 0)
-                 return i;
-         return 0;
+         int hor = chekerHor();
+         int ver = chekerVer();
+         if(hor != -1){
+             return hor;
+         }else if(ver != -1){
+             return ver;
+         }else 
+             return randomPos();
      }
+//-------------------Horizantal---------------------------
+        private int chekerHor(){
+            for(int start = 0 ; start < boxes.length; start+=3){
+               int counter = 0;
+               for(int i= start ; i < start + 3; i++){
+                    if(boxes[i].getCondition() == 1)
+                        counter++;
+                }
+               if(counter == 2){
+                   return findInLineHor(start);
+               }
+               counter = 0;
+            }
+            return -1;
+        }
+        
+        /*
+        * this method used if two elements of row is filled
+        * then this method find a position of empty position
+        */
+        private int findInLineHor(int start){
+           for(int i= start ; i < start + 3; i++)
+                if(boxes[i].getCondition() == 0)
+                    return i;
+            return -1;
+        }
+        
+//--------------Vertical----------------------
+        private int chekerVer(){
+           for(int start = 0 ; start < 3; start++){
+               int counter = 0;
+               for(int i= start ; i < start + 7; i+=3){
+                    if(boxes[i].getCondition() == 1)
+                        counter++;
+                }
+               if(counter == 2){
+                   return findInLineVer(start);
+               }
+               counter = 0;
+            }
+            return -1;
+        
+        } 
+                
+        private int findInLineVer(int start){
+           for(int i= start ; i < start + 7; i+=3){
+                if(boxes[i].getCondition() == 0)
+                    return i;
+           }
+            return -1;
+        }         
+                
+//--------------
+        
+        private int randomPos(){
+            while(true){
+                int res = (int)Math.round((Math.random()*8));
+                if(this.boxes[res].getCondition() == 0)
+                    return res;
+            }
+        }
+     
 }
